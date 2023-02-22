b8a2a685fa846bcffc5f9a39177a2b4cc0a55cce
YAKSHIT-22 <yakshitgarg25@gmail.com>
2023-02-22 17:22:58 +0530

    added background responsive changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import FaithMe from '../Img/FaithMe.png';
-import './Login.css';
+
 
 const Login = () => {
   return (
-    <div className='bg-gray-300'>
-      <div className="flex items-center justify-center min-h-[100vh]">
-        <div className="w-full max-w-[380px] py-[20px] px-[10px] min-[430px]:p-[30px] bg-white rounded-[30px] shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out flex flex-col justify-center items-center justify-items-center">
+    <div className='bg-primary bg-no-repeat bg-center'>
+      <div className="flex items-center justify-center h-screen p-4">
+        <div className="w-full max-w-[380px] py-[20px] px-[10px] min-[430px]:p-[2rem] bg-white rounded-[30px] shadow-md hover:shadow-lg cursor-pointer transition duration-300 ease-in-out flex flex-col justify-center items-center justify-self-center gap-4">
           <h1 className='font-semibold text-3xl mt-4'>WELCOME</h1>
-          <div className='mt-2 mb-3 text-md'><p className='text-gray-500 select-none cursor-default'>Don't have an account? <a className='text-black' href='#'>Register Now</a></p></div>
+          
           <form className='flex flex-col justify-items-center items-center w-[80%] mt-3 gap-5'>
-            <input type='text' placeholder='Username' id='username' className='w-full border border-gray-400 rounded-xl p-2'/>
-            <input type='password' placeholder='Password' id='password' className='w-full border border-gray-400 rounded-xl p-2 mb-3'/>
-            <button type='submit' className='w-[95%] shadow-md bg-orange-400 rounded-lg p-1.5 mb-5 hover:scale-[1.045] duration-300 transition ease-in-out'>Login</button>
+            <input type='text' placeholder='Username' id='username' className='w-full border border-gray-400 rounded-lg p-2'/>
+            <input type='password' placeholder='Password' id='password' className='w-full border border-gray-400 rounded-lg p-2 mb-3'/>
+            <button type='submit' className='w-[95%] shadow-md bg-orange-400 hover:bg-transparent hover:text-orange-400 hover:border font-semibold hover:border-orange-400 text-base rounded-lg p-2 mb-5 hover:scale-[1.045] duration-300 transition ease-in-out'>Login</button>
           </form>
+          <div className='mt-2 mb-3 text-md flex items-center justify-around w-full'><p className='text-gray-500 text-sm md:text-base select-none cursor-default'>Don't have an account?</p><a className='text-black text-sm md:text-base' href='/'>Register Now</a></div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'primary':"url('./Img/FaithMe.png')",
+      }
+    },
   },
   plugins: [],
 }
